1d8c8d56a9c3f74dace1be67f08e278809b92d5a
pappik <paparao.prema75@gmail.com>
2024-11-04 16:24:17 -0500

    loop testing for_each

--- Loops/loop.tf
+++ Loops/loop.tf
@@ -5,3 +5,20 @@ resource "null_resource" "loop-test" {
 
 
 
+#
+resource "null_resource" "loop-for_each" {
+  for_each = var.colours
+}
+
+variable "colours" {
+  default = {
+    red = 31
+    green = 32
+    yellow = 33
+    bluew = 34
+    magenta = 35
+    cyan = 36
+
+
+  }
+}
