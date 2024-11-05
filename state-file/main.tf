5b6732257045cddddf48a4203d2e6313218aabde
pappik <paparao.prema75@gmail.com>
2024-11-04 20:29:24 -0500

    multi env state file

---
+++ state-file/main.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "test/state-file"  ## this is the path we give of how things be store in this S3 bucket.
+                                ##so this creates a folder "test/state-file" and in that the state file is copied.
+    region = "us-east-1"
+  }
+}
