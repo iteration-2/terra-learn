24af1359ecf23d0c2c560fe82643030f825cd35c
pappik <paparao.prema75@gmail.com>
2024-11-05 18:43:31 -0500

    state-file s3 bucket test

--- state-file/main.tf
+++ state-file/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mybucket"
+    bucket = "yopappiyo"
     key    = "test/state-file"  ## this is the path we give of how things be store in this S3 bucket.
                                 ##so this creates a folder "test/state-file" and in that the state file is copied.
     region = "us-east-1"
